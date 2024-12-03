9376255da83778f60e84e11ef1330ef498cb38bb
Hussain Rana <hussainrana@Hussains-MacBook-Air.local>
2024-12-03 12:14:32 +0800

    Removed unnecessary header and footer components and updated gitignore files to include DS_Store

--- vendure-storefront/app/components/header/Header.tsx
+++ vendure-storefront/app/components/header/Header.tsx
@@ -29,18 +29,6 @@ export function Header({
       <div className="bg-zinc-100 text-gray-600 shadow-inner text-center text-sm py-2 px-2 xl:px-0">
         <div className="max-w-6xl mx-2 md:mx-auto flex items-center justify-between">
           <div>
-            <p className="hidden sm:block">
-              {t('vendure.exclusive')}{' '}
-              <a
-                href="https://funkyton.com/vendure-tutorial/"
-                target="_blank"
-                className="underline"
-              >
-                {t('vendure.repoLinkLabel')}
-              </a>
-            </p>
-          </div>
-          <div>
             <Link
               to={isSignedIn ? '/account' : '/sign-in'}
               className="flex space-x-1"
