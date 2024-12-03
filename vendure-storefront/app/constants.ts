9ad577de647d750229181e70048e566263ecb4db
Hussain Rana <hussainrana@Hussains-MacBook-Air.local>
2024-12-04 04:38:57 +0800

    Updated favicon and meta title and description of site

--- vendure-storefront/app/constants.ts
+++ vendure-storefront/app/constants.ts
@@ -1,6 +1,6 @@
-export const APP_META_TITLE = 'Vendure Remix Storefront';
+export const APP_META_TITLE = 'Double Nour Artistry';
 export const APP_META_DESCRIPTION =
-  'A headless commerce storefront starter kit built with Remix & Vendure';
+  'Custom tailor-made crafts';
 export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
 export let API_URL =
   typeof process !== 'undefined'
